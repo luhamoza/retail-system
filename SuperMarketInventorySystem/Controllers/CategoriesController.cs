9ec4931851657e6f192c8cf366a394c600a85bd6
luhamoza <luqmanhafizimohdzawawi@gmail.com>
2024-02-11 21:49:29 +0800

    first commit

---
+++ SuperMarketInventorySystem/Controllers/CategoriesController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SuperMarketInventorySystem.Controllers
+{
+    public class CategoriesController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Edit(int? id)
+        {
+            if (id.HasValue)
+            {
+                return new ContentResult() { Content = id.ToString() };
+            }
+            else
+            {
+                return new ContentResult() { Content = "null content" };
+            }
+        }
+    }
+}
---
+++ SuperMarketInventorySystem/Controllers/HomeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SuperMarketInventorySystem.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
