642991b7e43531a23cd14196144ccb559234022b
luhamoza <luqmanhafizimohdzawawi@gmail.com>
2024-02-14 00:17:22 +0800

    feat: add bootstrap, categoriesController, Views for Layout and other pages, and Models for categories

--- Controllers/CategoriesController.cs
+++ Controllers/CategoriesController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using RetailInvetorySystem.Models;
 
 namespace SuperMarketInventorySystem.Controllers
 {
@@ -10,14 +11,9 @@ namespace SuperMarketInventorySystem.Controllers
         }
         public IActionResult Edit(int? id)
         {
-            if (id.HasValue)
-            {
-                return new ContentResult() { Content = id.ToString() };
-            }
-            else
-            {
-                return new ContentResult() { Content = "null content" };
-            }
+            var category = new Category { CategoryId = id.HasValue ? id.Value : 0 };
+
+            return View(category);
         }
     }
 }
---
+++ Models/CategoriesRepository.cs
@@ -0,0 +1,60 @@
+﻿namespace RetailInvetorySystem.Models
+{
+    public static class CategoriesRepository
+    {
+        private static List<Category> _categories = new List<Category>()
+        {
+            new Category {CategoryId=1, Description="Daging", Name="Daging"},
+            new Category {CategoryId=2, Description="Sayur", Name="Sayur"},
+            new Category {CategoryId=3, Description="Cekedis", Name="Cekedis"},
+        };
+
+        public static void AddCategory(Category category)
+        {
+            var maxId = _categories.Max(c => c.CategoryId);
+            category.CategoryId = maxId + 1;
+            _categories.Add(category);
+        }
+
+        public static List<Category> GetCategories() => _categories;
+
+        public static Category? GetCategoryById(int categoryId)
+        {
+            var category = _categories.FirstOrDefault(x => x.CategoryId == categoryId);
+            if (category != null)
+            {
+                return new Category
+                {
+                    CategoryId = category.CategoryId,
+                    Description = category.Description,
+                    Name = category.Name,
+                };
+            }
+            else
+            {
+                return null;
+            }
+        }
+        public static void updateCategory(Category category, int categoryId)
+        {
+            if (categoryId != category.CategoryId)
+            {
+                return;
+            }
+            Category? categoryToUpdate = GetCategoryById(categoryId);
+            if (categoryToUpdate != null)
+            {
+                categoryToUpdate.Name = category.Name;
+                categoryToUpdate.Description = category.Description;
+            }
+        }
+        public static void deleteCategory(int categoryId)
+        {
+            Category? category = _categories.FirstOrDefault(x => x.CategoryId == categoryId);
+            if (category != null)
+            {
+                _categories.Remove(category);
+            }
+        }
+    }
+}
